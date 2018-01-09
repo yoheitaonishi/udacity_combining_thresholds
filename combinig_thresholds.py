689d440bdc327fcb3df5f3f91dcfc8c89da8d416
yoheitaonishi <onishiyoheita@gmail.com>
2018-01-09 11:42:06 +0900

    update file

--- combinig_thresholds.py
+++ combinig_thresholds.py
@@ -1,23 +1,95 @@
-def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
-    # Calculate directional gradient
-    # Apply threshold
-    return grad_binary
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pickle
+from IPython import embed
+from IPython.terminal.embed import InteractiveShellEmbed
+
+image = mpimg.imread('signs_vehicles_xygrad.png')
+
+def abs_sobel_thresh(image, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # 2) Take the derivative in x or y given orient = 'x' or 'y'
+    # 3) Take the absolute value of the derivative or gradient
+    if orient == 'x':
+        abs_sovel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+    if orient == 'y':
+        abs_sovel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1) )
+    # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
+    scaled_sovel = np.uint8(255*abs_sovel/np.max(abs_sovel))
+    # 5) Create a mask of 1's where the scaled gradient magnitude
+            # is > thresh_min and < thresh_max
+    binary_output = np.zeros_like(scaled_sovel)
+    binary_output[(scaled_sovel >= thresh[0]) & (thresh[1] >= scaled_sovel)] = 1
+    """
+    Numpy: Boolean or “mask” index arrays
+    >> y[np.array([0,2,4])]
+    array([[ 0,  1,  2,  3,  4,  5,  6],
+       [14, 15, 16, 17, 18, 19, 20],
+       [28, 29, 30, 31, 32, 33, 34]])
+    >> b = y>20
+    >> y[b]
+    array([21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34])
+    """
+    # 6) Return this mask as your binary_output image
+    return binary_output
 
 def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
-    # Calculate gradient magnitude
-    # Apply threshold
-    return mag_binary
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Calculate the magnitude 
+    grad_mag = np.sqrt(sobelx**2 + sobely**2)
+    # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+    scalar_factor = np.max(grad_mag)/255
+    grad_mag = (grad_mag/scalar_factor).astype(np.uint8)
+    # 5) Create a binary mask where mag thresholds are met
+    binary_output = np.zeros_like(grad_mag)
+    binary_output[(grad_mag >= mag_thresh[0]) & (mag_thresh[1] >= grad_mag)] = 1
+    # 6) Return this mask as your binary_output image
+    return binary_output
 
 def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
-    # Calculate gradient direction
-    # Apply threshold
-    return dir_binary
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Take the absolute value of the x and y gradients
+    # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+    # 5) Create a binary mask where direction thresholds are met
+    binary_output = np.zeros_like(absgraddir)
+    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
+    # 6) Return this mask as your binary_output image
+    return binary_output
 
 # Choose a Sobel kernel size
-ksize = 3 # Choose a larger odd number to smooth gradient measurements
+ksize = 11 # Choose a larger odd number to smooth gradient measurements
 
 # Apply each of the thresholding functions
-gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(0, 255))
-grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(0, 255))
-mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
-dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi/2))
+gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(30, 100))
+grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(30, 100))
+mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(30, 100))
+dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0.7, 1.3))
+
+# Combine Threshold
+combined = np.zeros_like(dir_binary)
+combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+
+# Plot the result
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(combined, cmap='gray')
+ax2.set_title('Thresholded Combine', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
